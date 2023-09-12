85806bfe70e6b0c94eb7d065ed4a91a485ec7245
BambiMC <fabians.viper@arcor.de>
2023-09-12 08:51:14 +0200

    working PSE

--- app/components/ElementTile/ElementTile.tsx
+++ app/components/ElementTile/ElementTile.tsx
@@ -1,7 +1,6 @@
 import { ElementData } from '@public/libraryChemistry';
 import React, { useState } from 'react';
 import ElementPopUp from '../ElementPopUp/ElementPopUp';
-import styles from './ElementTile.module.css'
 
 interface ElementTileProps {
   elementData?: ElementData;
@@ -17,9 +16,9 @@ const ElementTile: React.FC<ElementTileProps> = ({ elementData, addClasses }) =>
 
   return (
     <div
-      className={`text-xs md:text-xl xl:text-2xl text-center flex items-center justify-center text-black p-1 md:p-2 xl:p-4 2xl:p-8 md:border-2 md:border-black aspect-square ${addClasses} ${styles.ElementTile}`}
-      onClick={togglePopUp}
+      className={`text-xs md:text-xl xl:text-2xl text-center flex items-center justify-center text-black p-1 md:p-2 xl:p-4 2xl:p-8 md:border-2 md:border-black aspect-square hover:font-extrabold hover:outline sm:outline-4 outline-white ${addClasses}`}
       style={{ cursor: 'pointer' }}
+      onClick={togglePopUp}
     >
       {elementData ? elementData.kurzsymbol : '↓'}
       {isPopUpVisible && <ElementPopUp onClose={togglePopUp} elementData={elementData} addClasses="" />}
