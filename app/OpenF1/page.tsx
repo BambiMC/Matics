2dd4a49ea1703a97f3ab9a3644863f1b867c7147
fnberger <fabians.viper@arcor.de>
2024-09-03 08:27:43 +0200

    works for now

--- app/OpenF1/page.tsx
+++ app/OpenF1/page.tsx
@@ -1,27 +1,40 @@
 "use client";
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLanguageData } from '@data/languageLoader';
 import Button from '@app/components/Button/Button';
 
 //TODO lang einarbeitung: einmal überall open f1 in der projects seite miteinbauen und dann noch in der openf1 seite
 //TODO toggle Anzeige formattiert oder raw
+//TODO add clear text button
 
 const Homepage: React.FC = () => {
     const data = useLanguageData("openf1");
     const [responseData, setResponseData] = useState<any>(null);
-    const [queryText, setQueryText] = useState<string[]>(['driver_number=55&session_key=9159&speed>=300', 'driver_number=1&session_key=9158', 'session_key=9165&interval<0.005', 'session_key=9161&driver_number=63&lap_number=8', 'session_key=9161&driver_number=81&date>2023-09-16T13:03:35.200&date<2023-09-16T13:03:35.800', 'year=2023&country_name=Singapore', 'session_key=9158&pit_duration<31', 'meeting_key=1217&driver_number=40&position<=3', 'flag=BLACK AND WHITE&driver_number=1&date>=2023-01-01&date<2023-09-01', 'country_name=Belgium&session_name=Sprint&year=2023', 'session_key=9165&tyre_age_at_start>=3', 'session_key=9158&driver_number=11', 'meeting_key=1208&wind_direction>=130&track_temperature>=52']);
+    const [queryTexts, setQueryTexts] = useState<string[]>(['driver_number=55&session_key=9159&speed>=300', 'driver_number=1&session_key=9158', 'session_key=9165&interval<0.005', 'session_key=9161&driver_number=63&lap_number=8', 'session_key=9161&driver_number=81&date>2023-09-16T13:03:35.200&date<2023-09-16T13:03:35.800', 'year=2023&country_name=Singapore', 'session_key=9158&pit_duration<31', 'meeting_key=1217&driver_number=40&position<=3', 'flag=BLACK AND WHITE&driver_number=1&date>=2023-01-01&date<2023-09-01', 'country_name=Belgium&session_name=Sprint&year=2023', 'session_key=9165&tyre_age_at_start>=3', 'session_key=9158&driver_number=11', 'meeting_key=1208&wind_direction>=130&track_temperature>=52']);
 
     const apiItems = [['Car data', 'car_data'], ['Drivers', 'drivers'], ['Intervals', 'intervals'], ['Laps', 'laps'], ['Location', 'location'], ['Meetings', 'meetings'], ['Pit', 'pits'], ['Position', 'position'], ['Race Control', 'race_control'], ['Sessions', 'sessions'], ['Stints', 'stints'], ['Team radio', 'team_radio'], ['Weather', 'weather']];
-    const [selectedCategory, setSelectedCategory] = useState<string[]>([]);
+    const [selectedCategory, setSelectedCategory] = useState<string[]>(apiItems[0]);
 
     const attributeItems = [['brake', 'date', 'driver_number'], ['broadcast_name', 'country_code', 'driver_number'], ['date', 'driver_number', 'gap_to_leader']]
-    const [applicableAttributes, setApplicableAttributes] = useState<string[]>([]);
+    const [applicableAttributes, setApplicableAttributes] = useState<string[]>(attributeItems[0]);
 
+
+    const getQueryIndex = (): number => {
+        for (let i = 0; i < apiItems.length; i++) {
+            if (apiItems[i][0] === selectedCategory[0]) {
+                return i;
+            }
+        }
+        console.log("apiItems: ", apiItems);
+        console.log("selectedCategory: ", selectedCategory);
+        alert("ERROR WHILE GETTING QUERY INDEX");
+        return -1;
+    }
 
     const fetchData = async () => {
         try {
-            console.log(`https://api.openf1.org/v1/${selectedCategory[1]}?${queryText}`);
-            const response = await fetch(`https://api.openf1.org/v1/${selectedCategory[1]}?${queryText}`);
+            console.log(`https://api.openf1.org/v1/${selectedCategory[1]}?${queryTexts[getQueryIndex()]}`);
+            const response = await fetch(`https://api.openf1.org/v1/${selectedCategory[1]}?${queryTexts[getQueryIndex()]}`);
             if (!response.ok) {
                 throw new Error('Network response was not ok');
             }
@@ -34,13 +47,18 @@ const Homepage: React.FC = () => {
 
     const handleCategoryChange = (label: string[]) => {
         setSelectedCategory(label);
-        //setApplicableAttributes
-        const index: number = apiItems.indexOf(label);
-        setApplicableAttributes(attributeItems[index]);
+        setApplicableAttributes(attributeItems[getQueryIndex()]);
+        //TODO Load saved query?
+
     };
 
+    const handleQueryChange = (newQuery: string) => {
+        const modifiedQueryTexts = queryTexts;
+        modifiedQueryTexts[getQueryIndex()] = newQuery;
+        setQueryTexts(modifiedQueryTexts);
+    };
     const addAttribute = (label: string) => {
-
+        //TODO
     };
 
 
@@ -59,8 +77,8 @@ const Homepage: React.FC = () => {
                         <input
                             className='p-1 border-2 border-fnbg-orange rounded-lg w-full'
                             type="text"
-                            value={queryText}
-                            onChange={(e) => setQueryText(e.target.value)}
+                            value={queryTexts[getQueryIndex()]}
+                            onChange={(e) => handleQueryChange(e.target.value)}
                         />
                         <Button onClick={fetchData} addClasses='border-2 border-fnbg-orange ml-4'>Execute</Button>
                     </div>
